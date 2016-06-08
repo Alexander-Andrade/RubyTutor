6408c1701a3d148b8b7f2339bc1f0aaa5677c206
Alexander-Andrade <veimar.94@gmail.com>
2016-06-08 12:00:59 +0300

    return statement has been added

--- incorrect_array_remove_method/incorrect_array_remove_method.rb
+++ incorrect_array_remove_method/incorrect_array_remove_method.rb
@@ -13,10 +13,10 @@
 
 #removing all odd numbers from an array
 def remove_odd_numbers_from_array(a)
-  a.delete_if{|x| x % 2 == 1}
+  return a.delete_if{|x| x % 2 == 1}
 end
 
 
-a = [2,2,3,5,6,7,14,8,9,7,10,12]
+a = [2,2,3,5,7,1,6,7,14,8,9,7,10,12]
 
 print remove_odd_numbers_from_array(a)
